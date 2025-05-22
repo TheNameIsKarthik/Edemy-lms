0c03811f1c282dd3e1b5dc591675fb8d7c488fd3
TheNameIsKarthik <thenameiskarthik@pm.me>
2025-05-22 18:37:14 +0530

    educator ui done

--- client/src/components/educator/Footer.jsx
+++ client/src/components/educator/Footer.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { assets } from "../../assets/assets";
 
 const Footer = () => {
-  return <div>Footer Edu</div>;
+  return (
+    <footer className='flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-8 border-t '>
+      <div className='flex items-center gap-4'>
+        <img src={assets.logo} alt='logo' className='hidden md:block w-20' />
+        <div>
+          <p className='py-4 text-center text-xs md:text-sm text-gray-500'>Copyright 2025 &copy; SK. All Rights Reserved.</p>
+        </div>
+      </div>
+      <div className='flex items-center gap-3 max-md:mt-4'>
+        <a href='#'>
+          <img src={assets.facebook_icon} alt='facebook' />
+        </a>
+        <a href='#'>
+          <img src={assets.twitter_icon} alt='twitter' />
+        </a>
+        <a href='#'>
+          <img src={assets.instagram_icon} alt='instagram' />
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
